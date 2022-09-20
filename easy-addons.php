911304c2b7d85df1db099d8569964676998ff1af
Tanbir Ahmed <tanbirsylhet20@gmail.com>
2022-09-20 16:13:27 +0600

    released 2.0.0

--- easy-addons.php
+++ easy-addons.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy Addons
  * Plugin URI: https://github.com/bdtanbir/easy-addons
  * Description: Easy Addons for Elementor Is the Best Elementor Addons. Including Button, Flip Card and Team Member. More is coming soon.
- * Version: 1.0.1
+ * Version: 2.0.0
  * Author: Tanbir Ahmod
  * Author URI: https://github.com/bdtanbir
  * License: GPLv2
@@ -15,7 +15,7 @@
 
 defined('ABSPATH') || die();
 
-define('EASY_ADDONS_VERSION', '1.0.1');
+define('EASY_ADDONS_VERSION', '2.0.0');
 define('EASY_ADDONS__FILE__', __FILE__);
 define('EASY_ADDONS_DIR_PATH', plugin_dir_path(EASY_ADDONS__FILE__));
 define('EASY_ADDONS_DIR_URL', plugin_dir_url(EASY_ADDONS__FILE__));
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: addons, elementor addons, usefuladdons, elementor, easy addons, easy-addon
 Requires at least: 5.0
 Tested up to: 6.0.2
 Requires PHP: 5.7
-Stable tag: 1.0.1
+Stable tag: 2.0.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -34,6 +34,10 @@ More features and improvements are coming on regular updates.
 2. Team Members
 
 == Changelog ==
+
+= 2.0.0 =
+
+Added: Team member widget
 
 = 1.0.1 =
 
